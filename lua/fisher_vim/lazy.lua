029001af9ac5da6d644c18ec21bd9615ac0e40ba
Jacob Fisher <>
2024-03-18 03:30:09 +0100

    Remove treesitter playground since it's deprecated. Adding Harpoon2 as committed. It is broken right now for windows. Maybe fix, we see...

--- lua/fisher_vim/lazy.lua
+++ lua/fisher_vim/lazy.lua
@@ -16,31 +16,37 @@ vim.g.mapleader = ' '
 
 local plugins = {
     {
-        'nvim-telescope/telescope.nvim', version = '0.1.5',
-        dependencies = { {'nvim-lua/plenary.nvim'} }
+        'nvim-telescope/telescope.nvim',
+        version = '0.1.5',
+        dependencies = { { 'nvim-lua/plenary.nvim' } }
     },
     'EdenEast/nightfox.nvim',
-    'nvim-treesitter/nvim-treesitter', build = ':TSUpdate',
-    'nvim-treesitter/playground',
-    'theprimeagen/harpoon',
-    'mbbill/undotree',
-    'tpope/vim-fugitive',
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        dependencies = {
-            -- Manages the language servers from neovim
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
+    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
 
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'L3MON4D3/LuaSnip'},
-        },
+    "ThePrimeagen/harpoon",
+    -- {
+        -- "ThePrimeagen/harpoon",
+        -- branch = "harpoon2",
+        -- dependencies = { "nvim-lua/plenary.nvim" }
+        -- },
+        'mbbill/undotree',
+        'tpope/vim-fugitive',
+        {
+            'VonHeikemen/lsp-zero.nvim',
+            branch = 'v3.x',
+            dependencies = {
+                -- Manages the language servers from neovim
+                { 'williamboman/mason.nvim' },
+                { 'williamboman/mason-lspconfig.nvim' },
+
+                -- LSP Support
+                { 'neovim/nvim-lspconfig' },
+                -- Autocompletion
+                { 'hrsh7th/nvim-cmp' },
+                { 'hrsh7th/cmp-nvim-lsp' },
+                { 'L3MON4D3/LuaSnip' },
+            },
+        }
     }
-}
 
-require("lazy").setup(plugins, {})
+    require("lazy").setup(plugins, {})
