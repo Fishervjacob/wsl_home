9c63fef87aa71961354da751ee7605f176937c3f
Jacob Fisher <>
2024-03-19 20:17:44 +0100

    Remove unnecessary "mark" comment

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -3,7 +3,6 @@ local harpoon = require("harpoon")
 harpoon.setup()
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
---vim.keymap.set("n", "<leader>r", mark.rm_file)
 vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
