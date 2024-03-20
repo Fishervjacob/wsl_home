2e003ff6339cbbae0a5946f0909a2eaba2a9135e
Jacob Fisher <>
2024-03-20 03:31:28 +0100

    LSP working.

--- lua/fisher_vim/lazy.lua
+++ lua/fisher_vim/lazy.lua
@@ -32,18 +32,24 @@ local plugins = {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
         dependencies = {
-            -- Manages the language servers from neovim
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+            -- LSP support
+            'neovim/nvim-lspconfig',
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
 
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'L3MON4D3/LuaSnip' },
-        },
-    }
+            'hrsh7th/nvim-cmp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lua',
+
+            -- Snippets
+            'L3MON4D3/LuaSnip',
+            'rafamadriz/friendly-snippets'
+        }
+    },
 }
 
 require("lazy").setup(plugins, {})
